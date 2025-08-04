1ce8bd980ba250db3d5d6cc65f95c64b8c3f43e7
Muhammad Shareyar <muhammad.shareyar@yassir.com>
2025-08-04 10:03:36 +0200

    add community page

---
+++ app/not-found.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+const NotFoundPage = () => {
+  return (
+    <main className="not-found">
+      <h1>404</h1>
+      <p>Not found the resource! </p>
+    </main>
+  );
+};
+
+export default NotFoundPage;
