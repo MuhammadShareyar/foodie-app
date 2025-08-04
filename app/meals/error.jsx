5b90d0fbfce55b89e5aad257f4b892ae546e5289
Muhammad Shareyar <muhammad.shareyar@yassir.com>
2025-08-04 10:05:38 +0200

    meals page and fetching meals from database

---
+++ app/meals/error.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+const ErrorPage = () => {
+  return (
+    <main className="error">
+      <h1>Error!</h1>
+      <p>Oops something went wrong! </p>
+    </main>
+  );
+};
+
+export default ErrorPage;
---
+++ app/meals/loading-out.jsx
@@ -0,0 +1,9 @@
+import classes from "./loading.module.css"
+
+const LoadingPage = () => {
+  return (
+    <p className={classes.loading}>Loading Meals...</p>
+  )
+}
+
+export default LoadingPage
